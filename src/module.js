7fd92e337212c56aff1e536935237ffba2e7008a
Vladyslav Velychko <velychko@quintagroup.org>
2017-07-27 17:08:49 +0300

    Added directive and factory for the graph from angular-visjs lib

--- src/module.js
+++ src/module.js
@@ -54,6 +54,96 @@ export class GraphCtrl extends MetricsPanelCtrl {
 
 GraphCtrl.templateUrl = 'module.html';
 
+    angular.module('grafana.directives')
+    .directive('visNetwork', function() {
+        return {
+            restrict: 'EA',
+            transclude: false,
+            scope: {
+                data: '=',
+                options: '=',
+                events: '='
+            },
+            link: function(scope, element, attr) {
+                var networkEvents = [
+                    'click',
+                    'doubleClick',
+                    'oncontext',
+                    'hold',
+                    'release',
+                    'selectNode',
+                    'selectEdge',
+                    'deselectNode',
+                    'deselectEdge',
+                    'dragStart',
+                    'dragging',
+                    'dragEnd',
+                    'hoverNode',
+                    'blurNode',
+                    'zoom',
+                    'showPopup',
+                    'hidePopup',
+                    'startStabilizing',
+                    'stabilizationProgress',
+                    'stabilizationIterationsDone',
+                    'stabilized',
+                    'resize',
+                    'initRedraw',
+                    'beforeDrawing',
+                    'afterDrawing',
+                    'animationFinished'
+
+                ];
+
+                var network = null;
+
+                scope.$watch('data', function() {
+                    // Sanity check
+                    if (scope.data == null) {
+                        return;
+                    }
+
+                    // If we've actually changed the data set, then recreate the graph
+                    // We can always update the data by adding more data to the existing data set
+                    if (network != null) {
+                        network.destroy();
+                    }
+
+                    // Create the graph2d object
+                    network = new Network(element[0], scope.data, scope.options);
+
+                    // Attach an event handler if defined
+                    angular.forEach(scope.events, function(callback, event) {
+                        if (networkEvents.indexOf(String(event)) >= 0) {
+                            network.on(event, callback);
+                        }
+                    });
+
+                    // onLoad callback
+                    if (scope.events != null && scope.events.onload != null &&
+                        angular.isFunction(scope.events.onload)) {
+                        scope.events.onload(network);
+                    }
+                });
+
+                scope.$watchCollection('options', function(options) {
+                    if (network == null) {
+                        return;
+                    }
+                    network.setOptions(options);
+                });
+            }
+        };
+    });
+angular.module('grafana.services')
+    .factory('VisDataSet', function() {
+        'use strict';
+        return function(data, options) {
+            // Create the new dataSets
+            return new DataSet(data, options);
+        };
+    })
+
 
 
 export {
